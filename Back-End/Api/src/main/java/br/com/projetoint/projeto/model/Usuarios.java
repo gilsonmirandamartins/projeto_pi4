94d51d3661e1ad66ccceeba5723b700be37408ad
gefilho <161307411+gefilho@users.noreply.github.com>
2024-03-05 20:54:41 -0300

    Atualizaçoes não testadas na api
    falta testar

--- Back-End/Api/src/main/java/br/com/projetoint/projeto/model/Usuarios.java
+++ Back-End/Api/src/main/java/br/com/projetoint/projeto/model/Usuarios.java
@@ -1,14 +1,13 @@
 package br.com.projetoint.projeto.model;
 
-import java.time.LocalDate;
 import java.util.Date;
 
-import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Table;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "usuarios")
@@ -16,19 +15,25 @@ public class Usuarios {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
-	private Integer id;
-	@Column(name = "nome", length = 200, nullable = true)
-	private String nome;
-	@Column(name = "login", length = 15, nullable = true)
-	private String login;
-	@Column(name = "senha", length = 10, nullable = true)
-	private String senha;
+    private Integer id;
 
-	@Column(name = "dataNasc", nullable = false)
-	private Date dataNasc;
+    @Column(name = "nome", length = 200, nullable = false)
+    private String nome;
 
-	@Column(name = "dataRegistro", nullable = false)
-	private Date dataRegistro;
+    @Column(name = "login", length = 30, nullable = true)
+    private String login;
+
+    @Column(name = "senha", length = 10, nullable = false)
+    private String senha;
+
+    @Column(name = "dataNascimento", nullable = true)
+    private Date dataNascimento;
+
+    @Column(name = "dataCadastro", nullable = false)
+    private Date dataCadastro;
+
+    @Column(name = "ativo", nullable = false)
+    private Boolean ativo;
 
 	public Integer getId() {
 		return id;
@@ -62,20 +67,30 @@ public class Usuarios {
 		this.senha = senha;
 	}
 
-	public Date getDataNasc() {
-		return dataNasc;
+	public Date getDataNascimento() {
+		return dataNascimento;
 	}
 
-	public void setDataNasc(Date dataNasc) {
-		this.dataNasc = dataNasc;
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
 	}
 
-	public Date getDataRegistro() {
-		return dataRegistro;
+	public Date getDataCadastro() {
+		return dataCadastro;
 	}
 
-	public void setDataRegistro(Date dataRegistro) {
-		this.dataRegistro = dataRegistro;
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
 	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
 
 }
