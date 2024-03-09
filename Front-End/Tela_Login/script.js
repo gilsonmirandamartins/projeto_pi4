5366d0d749960adeeb9201bd3d6ce3eb6412ba4e
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-03-09 20:00:26 -0300

    Update Script da Tela de Login
    Ajuste da indentação do código e adição do script para o botão com a função de esconder a senha

--- Front-End/Tela_Login/script.js
+++ Front-End/Tela_Login/script.js
@@ -1,31 +1,43 @@
-    document.addEventListener('DOMContentLoaded', function () {
-        const form = document.querySelector("form");
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector("form");
 
-        form.addEventListener('submit', function (event) {
-            event.preventDefault();
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
 
-            const username = document.getElementById('in1').value;
-            const password = document.getElementById('in2').value;
+        const username = document.getElementById('in1').value;
+        const password = document.getElementById('in2').value;
 
-            // Enviar os dados de login para o backend
-            fetch(  'http://localhost:8081/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ login: username, senha: password })
-            })
-            .then(response => {
-                if (response.ok) {
-                    window.location.href = "http://127.0.0.1:5500/Front-End/tela_teste/teste_index.html"
-                } else {
-                    alert('Credenciais inválidas. Tente novamente.');
-                }
-            })
-            .catch(error => {
-                console.error('Erro ao fazer login:', error);
-                alert('Ocorreu um erro ao fazer login. Por favor, tente novamente mais tarde.');
-            });
+        // Enviar os dados de login para o backend
+        fetch(  'http://localhost:8081/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ login: username, senha: password })
+        })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = "http://127.0.0.1:5500/Front-End/tela_teste/teste_index.html"
+            } else {
+                alert('Credenciais inválidas. Tente novamente.');
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao fazer login:', error);
+            alert('Ocorreu um erro ao fazer login. Por favor, tente novamente mais tarde.');
         });
     });
+});
 
+function toggleSenha() {
+    var in2Input = document.getElementById("in2");
+    var icon = document.getElementById("icon");
+
+    if (in2Input.type === "password") {
+        in2Input.type = "text";
+        icon.classList.add("hide");
+    } else {
+        in2Input.type = "password";
+        icon.classList.remove("hide");
+    }
+}
