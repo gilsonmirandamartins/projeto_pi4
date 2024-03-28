d26b9114b7104ae7de8f1214858e130fe59d5489
gefilho <161307411+gefilho@users.noreply.github.com>
2024-03-28 10:49:22 -0300

    .

---
+++ Back-End/Api/src/main/java/br/com/projetoint/projeto/controller/UserRepository.java
@@ -0,0 +1,17 @@
+package br.com.projetoint.projeto.controller;
+
+import br.com.projetoint.projeto.model.Usuarios;
+
+public class UserRepository {
+
+    public static Usuarios findByLogin(String login) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findByLogin'");
+    }
+
+    public static void save(Usuarios usuarioExistente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+}
