6acfd325a6aeeba490ee3e9c3d30920608f9d22e
gefilho <161307411+gefilho@users.noreply.github.com>
2024-05-06 15:59:29 -0300

    .

--- Scripts agendamento.sql
+++ Scripts agendamento.sql
@@ -6,18 +6,16 @@ CREATE TABLE Medico (
 );
 
 
-CREATE TABLE Agendamento (
-    idAgendamento INT AUTO_INCREMENT PRIMARY KEY,
-    nomePaciente VARCHAR(100),
-    email VARCHAR(100),
-    status ENUM('agendado', 'cancelado', 'realizado') DEFAULT 'agendado',
-    clinica ENUM('Gastroclínica', 'Otoclínica', 'Medical Center'),
-    idMedico INT,
-    dataHoraAgendamento DATETIME,
-    dataCadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (idMedico) REFERENCES Medico(idMedico),
-    UNIQUE (clinica, dataHoraAgendamento, idMedico)
-);
+CREATE TABLE `agendamento` (
+  `idAgendamento` int NOT NULL AUTO_INCREMENT,
+  `nomePaciente` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `status` enum('AGENDADO','CANCELADO','REALIZADO') NOT NULL DEFAULT 'AGENDADO',
+  `clinica` enum('Medical Center','Otomédica','Otoclínica') NOT NULL,
+  `idMedico` int NOT NULL,
+  `dataHoraAgendamento` datetime NOT NULL,
+  `dataCadastro` datetime NOT NULL,
+  FOREIGN KEY (`idMedico`) REFERENCES `medico` (`idMedico`)
 
 
 
