55800254ca174d9964df81389b1af763ff1880f4
gefilho <161307411+gefilho@users.noreply.github.com>
2024-05-06 15:09:05 -0300

    crud de medico
    falta o delete

--- Back-End/Api/src/main/java/br/com/projetoint/projeto/DAO/MedicoRepository.java
+++ Back-End/Api/src/main/java/br/com/projetoint/projeto/DAO/MedicoRepository.java
@@ -1,9 +1,13 @@
 
 package br.com.projetoint.projeto.DAO;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.projetoint.projeto.model.Medico;
 
 public interface MedicoRepository extends JpaRepository<Medico, Integer> {
+
+    Optional<Medico> findByNomeMedico(String nome);
 }
 
--- Back-End/Api/src/main/java/br/com/projetoint/projeto/controller/MedicoController.java
+++ Back-End/Api/src/main/java/br/com/projetoint/projeto/controller/MedicoController.java
@@ -1,12 +1,19 @@
 package br.com.projetoint.projeto.controller;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projetoint.projeto.DAO.MedicoRepository;
@@ -19,18 +26,70 @@ public class MedicoController {
     @Autowired
     private MedicoRepository medicoRepository;
 
+    @GetMapping("/verificar") // login de medico, verificar id e nome, endpoint: /medico/verificar
+    public ResponseEntity<String> verificarMedico(@RequestParam int id, @RequestParam String nome) {
+        Optional<Medico> medicoOptional = medicoRepository.findById(id);
+
+        if (medicoOptional.isPresent()) {
+            Medico medico = medicoOptional.get();
+            if (medico.getNomeMedico().equalsIgnoreCase(nome)) {
+                return ResponseEntity.ok("Verificado com sucesso");
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Nome do médico não corresponde");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Médico não encontrado");
+        }
+    }
+
+    @PostMapping("/criar") // criar medico.Informe o medico e o id é criado automatico,endpoint:/medico/criar
+
+    public ResponseEntity<String> criarMedico(@RequestBody Medico medico) {
+        Medico medicoSalvo = medicoRepository.save(medico);
+        String mensagem = String.format("Médico '%s' criado com sucesso. Seu ID é %d.",
+                medicoSalvo.getNomeMedico(),
+                medicoSalvo.getIdMedico());
+        return ResponseEntity.ok(mensagem);
+    }
+
+    @GetMapping("/listar")
+    public List<Medico> listarMedicos() {
+        return medicoRepository.findAll();
+    }
+
+    @PutMapping("/editar") // edita apenas o nome do usuario atraves do nome atual,endpoint:/medico/editar
+
+    public ResponseEntity<String> editarMedico(@RequestParam int idAtual, @RequestBody Medico medicoAtualizado) {
+        Optional<Medico> medicoOptional = medicoRepository.findById(idAtual);
+        if (medicoOptional.isPresent()) {
+            Medico medicoExistente = medicoOptional.get();
+            medicoExistente.setNomeMedico(medicoAtualizado.getNomeMedico());
+            medicoRepository.save(medicoExistente);
+            return ResponseEntity.ok("Médico atualizado com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Médico não encontrado.");
+        }
+    }
+
+    @DeleteMapping("/deletar")
+    public ResponseEntity<String> deletarMedico(@RequestBody Medico medico) {
+        // Buscar o médico pelo nome
+        Optional<Medico> medicoOptional = medicoRepository.findByNomeMedico(medico.getNomeMedico());
+
+        if (medicoOptional.isPresent()) {
+            // Se o médico foi encontrado, exclua-o
+            medicoRepository.delete(medicoOptional.get());
+            return ResponseEntity.ok("Médico '" + medico.getNomeMedico() + "' excluído com sucesso.");
+        } else {
+            // Se o médico não foi encontrado, retorne um erro
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Médico com nome '" + medico.getNomeMedico() + "' não encontrado.");
+        }
+    }
+
     @GetMapping("/{id}")
     public Medico obterMedicoPorId(@PathVariable int id) {
         return medicoRepository.findById(id).orElse(null);
     }
 
-    @GetMapping
-    public List<Medico> listarMedicos() {
-        return medicoRepository.findAll();
-    }
-
-    @PostMapping
-    public Medico criarMedico(@RequestBody Medico medico) {
-        return medicoRepository.save(medico);
-    }
 }
