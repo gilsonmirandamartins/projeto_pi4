cff2bcb230270ab4c94d4478976afb4e0bc1de89
gefilho <161307411+gefilho@users.noreply.github.com>
2024-06-03 13:05:13 -0300

    Update crudPaciente.js

--- Front-End/Home/Telas_Paciente/crudPaciente.js
+++ Front-End/Home/Telas_Paciente/crudPaciente.js
@@ -80,6 +80,55 @@ document.addEventListener('DOMContentLoaded', () => {
     formLogin?.addEventListener('submit', loginPaciente);
     // Função para login de paciente - Fim
 
+    //Função para edição de paciente - Início
+    const formEditar = document.getElementById('formEditarPaciente');
+
+    const editarPaciente = async (event) => {
+        event.preventDefault();
+
+        const idPaciente = document.getElementById('idPaciente').value;
+        const novoNome = document.getElementById('novoNome').value;
+        const novoDocumento = document.getElementById('novoDocumento').value;
+        const novoSexo = document.getElementById('novoSexo').value;
+        const novaDataNascimento = document.getElementById('novaDataNascimento').value;
+
+        const data = {
+            nome: novoNome,
+            documento: novoDocumento,
+            sexo: novoSexo,
+            dataNascimento: novaDataNascimento
+        };
+
+        try {
+            const response = await fetch(`http://localhost:8081/paciente/editar/${idPaciente}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            const mensagem = document.getElementById('mensagem');
+            if (response.ok) {
+                const respostaTexto = await response.text();
+                mensagem.textContent = respostaTexto;
+                mensagem.style.color = 'green';
+            } else {
+                const errorText = await response.text();
+                console.error('Erro na resposta:', errorText);
+                mensagem.textContent = 'Falha ao editar paciente.';
+                mensagem.style.color = 'red';
+            }
+        } catch (error) {
+            console.error('Erro na requisição:', error);
+            const mensagem = document.getElementById('mensagem');
+            mensagem.textContent = 'Erro de conexão.';
+            mensagem.style.color = 'red';
+        }
+    };
+
+    formEditar?.addEventListener('submit', editarPaciente);
+
     // Mensagem de boas-vindas - Início
     const mensagemBoasVindas = localStorage.getItem('mensagemBoasVindas');
     if (mensagemBoasVindas) {
